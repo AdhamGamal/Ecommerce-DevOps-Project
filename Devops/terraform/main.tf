203881bed08acbd888d570359d106066ae758b99
AdhamGamal <adhammohamadgamal@yahoo.com>
2025-04-08 11:33:27 +0200

    update ec2 name

--- Devops/terraform/main.tf
+++ Devops/terraform/main.tf
@@ -17,7 +17,17 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
 
