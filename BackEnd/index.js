2b3541ee0f5a5ad6f1bebec27fd3d50a10c924b0
AdhamGamal <adhammohamadgamal@yahoo.com>
2025-04-09 16:07:20 +0200

    update containerization pipeline

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -57,11 +57,12 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(+process.env.PORT, (error) => {
+app.listen(process.env.PORT || 8000, (error) => {
   if (!error)
     console.log(
       "Server is Successfully Running, and App is listening on port " +
-        +process.env.PORT
+        (process.env.PORT || 8000)
     );
-  else console.log("Error occurred, server can't start", error);
+  else
+    console.log("Error occurred, server can't start", error);
 });
