9d6ae25f3edc735ce892ae8d10970befeb4b2126
AdhamGamal <adhammohamadgamal@yahoo.com>
2025-04-14 20:27:03 +0200

    correct output security group name

--- Devops/terraform/outputs.tf
+++ Devops/terraform/outputs.tf
@@ -14,8 +14,14 @@ output "frontend_instance" {
     instance_id = aws_instance.elegance_frontend.id
   }
 }
+# To reference the backend security group
+output "backend_security_group_id" {
+  description = "The ID of the backend security group"
+  value       = aws_security_group.backend_sg.id
+}
 
-output "security_group_id" {
-  description = "Security group ID"
-  value       = aws_security_group.elegance_sg.id
+# To reference the frontend security group
+output "frontend_security_group_id" {
+  description = "The ID of the frontend security group"
+  value       = aws_security_group.frontend_sg.id
 }
