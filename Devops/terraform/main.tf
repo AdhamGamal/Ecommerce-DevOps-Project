15e683897aac34dcde0f8ba07813fabc7115ea57
AdhamGamal <adhammohamadgamal@yahoo.com>
2025-04-16 23:06:15 +0200

    add http port to frontend

--- Devops/terraform/main.tf
+++ Devops/terraform/main.tf
@@ -67,6 +67,22 @@ resource "aws_security_group" "frontend_sg" {
   }
 
   ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "Prometheus"
     from_port   = 9090
     to_port     = 9090
@@ -99,6 +115,22 @@ resource "aws_security_group" "backend_sg" {
   name        = "backend-sg"
   description = "Security group for backend services"
   vpc_id      = aws_vpc.elegance_vpc.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     description = "SSH"
